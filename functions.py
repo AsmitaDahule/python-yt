aeb616424bc4befbb39b08df7c246a2639c86f4c
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-12 16:00:37 +0530

    arguments

--- functions.py
+++ functions.py
@@ -3,3 +3,30 @@ def sayHello(name):
 
 str = sayHello("asmi")
 print(str)  # Output: Hello, asmi!
+
+# arguments
+# def sum(num1, num2):
+#     sum = num1 + num2
+#     print(sum)
+
+# sum(2,3)
+
+# def sum(num1, num2=5):
+#     sum = num1 + num2
+#     print(sum)
+
+# sum(2)
+
+# def sum(*num):
+#     sum = 0
+#     for i in num:
+#         sum = sum + i
+#     print(sum)
+
+# sum(1,2,3,4)
+
+# def sum(**kwargs):
+#     for key, value in kwargs.items():
+#         print(key, value)
+
+# sum(name="asmi", age=21, mail="asmi@gmail.in")
