dd1aaa49275505fd65de6ebb5e2dd575bfe7acf6
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-13 16:13:57 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+mob_num = {
+  "asmi": 32154,
+  "chandu": 4566,
+  "shweta": 8748,
+  "dhano" : 4855,
+}
+
+print(mob_num)
+
+del mob_num["asmi"]
+print(mob_num)
+
+mob_num["kiddo"] = "dear zindgi"
+print(mob_num)
