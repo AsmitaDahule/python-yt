8cf90bce2dcd08a9e6405f6526cff66da1365e25
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-24 19:40:17 +0530

    datatypes

--- day_05.py
+++ day_05.py
@@ -7,3 +7,6 @@ print("this is me asmi \nand i'm 22 year old")
 print(''' triple quote ''')
 
 print("\"asmi said\": I will win ")
+
+print("asmi", 22, 2026, sep="~")  # interesting
+print("asmi", 22, 2026, end="009\n")  
---
+++ day_06.py
@@ -0,0 +1,8 @@
+a = 22
+print(a)
+
+b = "hello"
+print(b)
+
+c = 8
+print(type(a + c))
