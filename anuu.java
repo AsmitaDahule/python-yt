c7bc57d70f788c536b852596945d84c24d828ab0
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-24 20:22:11 +0530

    list and tuples

---
+++ anuu.java
@@ -0,0 +1,9 @@
+class Anuu {
+  public static void main(String args[]) {
+    int i = 10;
+    while(i == 0) {
+      System.out.print("Anuu  ");
+    }
+    i--;
+  }
+}
--- day_06.py
+++ day_06.py
@@ -4,5 +4,11 @@ print(a)
 b = "hello"
 print(b)
 
-c = 8
+c = 8.5
 print(type(a + c))
+
+tuple1 = (("asmi", "anuu"),("anii", "anju"))
+print(tuple1)
+
+list1 = [["asmita", "anuuuu"],["titu", "samy"]]
+print(list1)
