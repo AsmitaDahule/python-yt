ece3c65726d6c358124c0f412b2a843587eb85d2
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-24 19:12:32 +0530

    remaining basic

--- day_05.py
+++ day_05.py
@@ -3,3 +3,7 @@ print("this is me asmi \nand i'm 22 year old")
 # print("this is me asmi \nand i'm 22 year old")
 # print("this is me asmi \nand i'm 22 year old")
 # print("this is me asmi \nand i'm 22 year old")
+
+print(''' triple quote ''')
+
+print("\"asmi said\": I will win ")
