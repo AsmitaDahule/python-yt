86d3a62ad32efb5a0a0196475ca5bb78a264898e
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-26 18:49:15 +0530

    string methods

---
+++ day_09.py
@@ -0,0 +1,15 @@
+# string methods
+
+name = "Asmi Dahule"
+print(len(name))
+print(name[0:4])
+print(name[5:])
+print(name.lower())
+print(name.upper())
+print(name[:5])
+print(name[0:-4])  # 11 - 4 = 7
+
+
+# quick quiz
+nm = "Harry"
+print(nm[-4:-2])
