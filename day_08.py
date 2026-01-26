a1df3cb5955b387bd5cbd781468581cb1a781780
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-26 17:01:21 +0530

    multi line string

--- day_08.py
+++ day_08.py
@@ -6,3 +6,12 @@ print(name)
 print(name2)
 
 print("Hello " + name)
+
+multiString = ''' hi asmi,
+what's going on bro..
+i hope you are doing well. '''
+
+print(multiString[0])
+
+for char in multiString:
+    print(char)
