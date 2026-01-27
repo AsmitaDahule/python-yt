0fa1c08797ed8eb703bb8647715eb2f0fb6e8b52
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-27 17:24:37 +0530

    list

--- day_18.py
+++ day_18.py
@@ -1,5 +1,16 @@
-def avg(a=10, b=5):
+def avg(a,b):
     print("average is ", (a+b)/2 )
+    print(a,b)
 
 
-avg(6)
+avg(b=10, a=5)
+
+
+def sum(*numbers):
+    sum = 0
+    for i in numbers:
+        sum = sum + i
+    print("sum is ", sum)
+    print(type(numbers))
+
+sum(1,2,3,4,5,6,7,8,9,10)
---
+++ day_19.py
@@ -0,0 +1,5 @@
+# list
+l = [1,"asmi",3,4,5]
+print("list:", l)
+print("type of list:", type(l))
+print(l[0])
