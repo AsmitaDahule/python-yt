3b6c2b4f8c16e5064ec64f80d492d1fc63e41007
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-27 19:18:58 +0530

    tuple

---
+++ day_21.py
@@ -0,0 +1,7 @@
+# tuple
+tup = (1,2,3)
+print(type(tup), tup)
+
+print(tup[1])
+# print(tup[5]) #
+print(tup[-2])
---
+++ day_22.py
@@ -0,0 +1,4 @@
+tuple1 = (0,1,2,3,4,5,6,7,8,9)
+res = tuple1.index(5,2,10)
+print(res)
+print(len(tuple1))
