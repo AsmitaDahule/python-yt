a541fa100184304e991678225306e08f4030af26
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-27 07:19:07 +0530

    for loop

---
+++ day_14.py
@@ -0,0 +1,12 @@
+# for loop
+# colors = ['red', 'green', 'blue']
+# for color in colors:
+#     print(color)
+#     for letter in color:
+#         print(letter)
+
+
+for k in range(3):
+    print("Outer loop iteration:", k)
+    for j in range(2):
+        print("  Inner loop iteration:", j)
