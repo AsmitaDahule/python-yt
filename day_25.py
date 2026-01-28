1eb7b6e1ba2ebc48a512b7c485233bc851f5186d
Asmita Dahule <dahuleasmita@gmail.com>
2026-01-28 06:16:21 +0530

    fibonacci series

--- day_25.py
+++ day_25.py
@@ -6,3 +6,17 @@ def factorial(n):
     
 res = factorial(5)
 print(res)
+
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+fib = fibonacci(10)
+print(fib)
+
+
+n = 10
+for i in range(n):
+    print(fibonacci(i), end=" ")
